dfc89b71d78510a912439d3b9b2c958ef9332997
Matistik <Lancer888-123>
2021-02-28 20:31:42 +0100

    uloha a2 - mojePole

--- src/main/java/sk/stuba/fei/oop/Cvicenie1a2.java
+++ src/main/java/sk/stuba/fei/oop/Cvicenie1a2.java
@@ -5,79 +5,27 @@ import java.util.Scanner;
 
 public class Cvicenie1a2 {
     public static void main(String[] args) {
+        System.out.println("by matistikoff\n");
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("hello word");
+        //program 2a
+        System.out.println("zadaj velkost pola: ");
+        int velkost = keyboard.nextInt();
+        int[] mojePole = new int[velkost];
 
-        int Prve = 5;
-        int Druhe = 5;
-        System.out.println(porovnaj(Prve,Druhe));
-
-        //horoshop
-        int[] Mesiace = {0,1, 32, 61, 92,122,153,183,214,245,275,306,336};
-        int i = 0;
-        while (i == 0){
-
-            System.out.println("zadaj den: ");
-            int Den = keyboard.nextInt();
-            System.out.println("Zadaj poradove cislo mesiaca : ");
-            int Mesiac = keyboard.nextInt();
-
-            int Horoskop = Mesiace[Mesiac] + Den;
-            System.out.println(Horoskop);
-
-             if ((Horoskop >= 20) && (Horoskop <= 49)){
-                System.out.println("Vodnar");
-            }
-             else if ((Horoskop >= 50) && (Horoskop <= 80)){
-                 System.out.println("Ryby");
-             }
-             else if ((Horoskop >= 81) && (Horoskop <= 111)){
-                System.out.println("Baran");
-            }
-            else if ((Horoskop >= 112) && (Horoskop <= 141 )){
-                System.out.println("Byk");
-            }
-            else if ((Horoskop >= 142) && (Horoskop <= 173)){
-                System.out.println("Blizenci");
-            }
-            else if ((Horoskop >= 174) && (Horoskop <= 204)){
-                System.out.println("Rak");
-            }
-            else if ((Horoskop >= 205) && (Horoskop <= 235)){
-                System.out.println("Lev");
-            }
-            else if ((Horoskop >= 236) && (Horoskop <= 266)){
-                System.out.println("Panna");
-            }
-            else if ((Horoskop >= 267) && (Horoskop <= 296)){
-                System.out.println("Vahy");
-            }
-            else if ((Horoskop >= 297) && (Horoskop <= 326)){
-                System.out.println("Skorpion");
-            }
-            else if ((Horoskop >= 327) && (Horoskop <= 356)){
-                System.out.println("Strelec");
-            }
-            else {
-                System.out.println("Kozorozec");
-            }
-            System.out.println("Ak si chceš program zopakovať stlač 1! \nPre ukončenie programu stlač 0!");
-            int program = keyboard.nextInt();
-            if (program == 0){
-                i++;
-                System.out.println("Koniec programu");
-            }
-            else if (program > 1 || program < 0){
-                System.out.println("Stlacil si nespravne cislo! \nAk si chceš program zopakovať stlač 1! \nPre ukončenie programu stlač 0!");
-            }
+        for (int i = 0; i < mojePole.length; i++) {
+            System.out.println("zadaj cislo na " + i + " indexe");
+            int cislo = keyboard.nextInt();
+            mojePole[i] = cislo;
         }
+        for (int i = 0; i < mojePole.length;i++){
+            System.out.println("cislo na indexe " + i + " je " + mojePole[i]);
+        }
+        System.out.println("pole sa nachadza na adrese " + mojePole); //vypise adresu pola
 
-
-    }
-
-    public static int porovnaj(int a, int b) {
-        return Integer.compare(a, b);
     }
 }
+
+
+
 
 
