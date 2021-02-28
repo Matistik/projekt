167ebf6793d581c6ce9a5bb85ecd769f2de1b2d7
Matistik <Lancer888-123>
2021-02-28 20:40:06 +0100

    incializacia pola

--- src/main/java/sk/stuba/fei/oop/Cvicenie1a2.java
+++ src/main/java/sk/stuba/fei/oop/Cvicenie1a2.java
@@ -7,21 +7,12 @@ public class Cvicenie1a2 {
     public static void main(String[] args) {
         System.out.println("by matistikoff\n");
         Scanner keyboard = new Scanner(System.in);
-        //program 2a
-        System.out.println("zadaj velkost pola: ");
-        int velkost = keyboard.nextInt();
-        int[] mojePole = new int[velkost];
-
-        for (int i = 0; i < mojePole.length; i++) {
-            System.out.println("zadaj cislo na " + i + " indexe");
-            int cislo = keyboard.nextInt();
-            mojePole[i] = cislo;
+        //program 2b
+        int [] pole = new int[] {1,2,3,4,5}; //inicializacia pola
+        pole[2] = 10;
+        for (int i = 0; i < pole.length;i++){
+            System.out.println(pole[i]);
         }
-        for (int i = 0; i < mojePole.length;i++){
-            System.out.println("cislo na indexe " + i + " je " + mojePole[i]);
-        }
-        System.out.println("pole sa nachadza na adrese " + mojePole); //vypise adresu pola
-
     }
 }
 
