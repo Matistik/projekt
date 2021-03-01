21a5f1a54a370cf0d47d42f4342d0171e1017872
Matistik <Lancer888-123>
2021-03-01 20:22:51 +0100

    metody

--- src/main/java/sk/stuba/fei/oop/Cvicenie1a2.java
+++ src/main/java/sk/stuba/fei/oop/Cvicenie1a2.java
@@ -4,22 +4,17 @@ import sk.stuba.fei.oop.car.Auto;
 
 public class Cvicenie1a2 {
     public static void main(String[] args) {
-        Auto skoda = new Auto();
-        Auto bmw = new Auto();
+       Auto skoda = new Auto();
 
-        skoda.one = 1;
-        bmw.one = 2;
+       skoda.stavNadrze = 100;
 
-        if(skoda.one == bmw.one){
-            System.out.println("rovnake");
-        } else {
-            System.out.println("rozne");
-        }
+       skoda.drive(100);
 
+        System.out.println(skoda.stavNadrze);
 
+        skoda.drive(220);
 
-
-
+        System.out.println(skoda.stavNadrze);
 
 
         }
--- src/main/java/sk/stuba/fei/oop/car/Auto.java
+++ src/main/java/sk/stuba/fei/oop/car/Auto.java
@@ -1,13 +1,18 @@
 package sk.stuba.fei.oop.car;
 
 public class Auto {
-    public int pocetKolies;
-    public int pocetDvery;
-    public double kapacitaNadrze;
-    public String nazov;
 
-    public int one; //verejne, mozem upravovat
-    int two; //iba v tom isto package, malo sa pouziva
-    protected int three;
-    private int four; //nemam pristup
+    private static final double FUEL_PER_KM= 0.5; //pri konstatne davam static, ked je final neda sa menit
+
+    public double kapacitaNadrze;
+    public double stavNadrze;
+
+    public void drive(double distanceInKm){
+       // this.stavNadrze -= distanceInKm * Auto.FUEL_PER_KM;
+        stavNadrze -= distanceInKm * FUEL_PER_KM;
+        if(stavNadrze < 0){
+            stavNadrze = 0;
+        }
+
+    }
 }
