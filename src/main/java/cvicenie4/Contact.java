4b42f558912409625f344d0c66572aa735a5deed
Matistik <Lancer888-123>
2021-03-08 21:05:08 +0100

    object references

--- src/main/java/cvicenie4/Contact.java
+++ src/main/java/cvicenie4/Contact.java
@@ -2,11 +2,37 @@ package cvicenie4;
 
 public class Contact {
 
-    String firstName;
-    String surname;
+    private String firstName;
+    private String surname;
 
     public Contact(String firstName, String surname) {
         this.firstName = firstName;
         this.surname = surname;
+    }
+
+    private Contact[] friends;
+
+    public Contact(String fullName){
+        var parts = fullName.split(" ");
+        firstName = parts[0];
+        surname = parts[1];
+    }
+
+    public static Contact parseFromFullName(String fullName){
+        return new Contact(fullName);
+
+    }
+
+    public void setFriends(Contact...newFriends){
+        this.friends = newFriends;
+
+
+    }
+    public void setFriendsFromArray(Contact[] newFriends){
+    this.friends = newFriends;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 }
--- src/main/java/cvicenie4/Main.java
+++ src/main/java/cvicenie4/Main.java
@@ -5,7 +5,24 @@ public class Main {
     public static void main(String[] args) {
         Contact contact = new Contact("Miso", "Matistik");
         var contact2 = new Contact("Martin", "Kranec");
-        var cislo = 5;
+        var contact3 = new Contact("Tomas", "Kovacik");
+
+        var pat = Integer.parseInt("5");
+
+        var contact4 = Contact.parseFromFullName("Gabriel Juhas");
+
+        var contacts1 = new Contact[] {contact2,contact3,contact4};
+        contact.setFriendsFromArray(contacts1);
+
+        contacts1[0].setFirstName("Mato");
+
+        contact.setFriends(contact2,contact3,contact4);
+
+        contact3.setFirstName("kovy");
+
+
 
     }
+
+
 }
