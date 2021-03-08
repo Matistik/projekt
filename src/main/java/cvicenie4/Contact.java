3c85ee7cacac3d391597b0f53fdd340e5274b634
Matistik <Lancer888-123>
2021-03-08 20:39:55 +0100

    var variables

---
+++ src/main/java/cvicenie4/Contact.java
@@ -0,0 +1,12 @@
+package cvicenie4;
+
+public class Contact {
+
+    String firstName;
+    String surname;
+
+    public Contact(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+}
---
+++ src/main/java/cvicenie4/Main.java
@@ -0,0 +1,11 @@
+package cvicenie4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Contact contact = new Contact("Miso", "Matistik");
+        var contact2 = new Contact("Martin", "Kranec");
+        var cislo = 5;
+
+    }
+}
